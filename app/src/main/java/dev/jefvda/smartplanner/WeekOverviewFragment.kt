3dc4b813fd30dbb03d2dbf79c37cb05d9c60934d
JefvdA <jef.v.d.a@live.be>
2022-07-07 23:02:16 +0200

    feat: updated WeekOverviewFragment
    now also displays new actionbar title.
    For now, the week number displayed is a hard-coded value

--- app/src/main/java/dev/jefvda/smartplanner/WeekOverviewFragment.kt
+++ app/src/main/java/dev/jefvda/smartplanner/WeekOverviewFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import dev.jefvda.smartplanner.databinding.FragmentWeekOverviewBinding
 
 /**
@@ -21,15 +22,20 @@ class WeekOverviewFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentWeekOverviewBinding.inflate(inflater, container, false)
-        return binding.root
 
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.week_overview_toolbar_title, 26)
     }
 
     override fun onDestroyView() {
