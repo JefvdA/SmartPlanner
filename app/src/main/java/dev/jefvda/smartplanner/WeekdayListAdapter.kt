9bd84606bc47989522067eb9c3631cc7ba792898
JefvdA <jef.v.d.a@live.be>
2022-07-07 23:23:09 +0200

    fix: add ViewHolder and Adapter for Weekday

---
+++ app/src/main/java/dev/jefvda/smartplanner/WeekdayListAdapter.kt
@@ -0,0 +1,19 @@
+package dev.jefvda.smartplanner
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class WeekdayListAdapter(var weekdayList: MutableList<Weekday>): RecyclerView.Adapter<WeekdayViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeekdayViewHolder {
+        return WeekdayViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.weekday_list_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: WeekdayViewHolder, position: Int) {
+        holder.populateViewHolder(weekdayList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return weekdayList.size
+    }
+}
