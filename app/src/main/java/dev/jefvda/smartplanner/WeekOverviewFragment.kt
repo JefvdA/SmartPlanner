fea9ed68cf05a473cadcd0aedc493f1e624d6d45
JefvdA <jef.v.d.a@live.be>
2022-07-07 23:29:46 +0200

    feat: initialize weekdayList
    Added bindings to weekdayRecyclerView and weekdayListAdapter.
    
    Also updated WeekdayListAdapter to initialize the weekdays.

--- app/src/main/java/dev/jefvda/smartplanner/WeekOverviewFragment.kt
+++ app/src/main/java/dev/jefvda/smartplanner/WeekOverviewFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dev.jefvda.smartplanner.databinding.FragmentWeekOverviewBinding
 
 /**
@@ -14,6 +16,10 @@ import dev.jefvda.smartplanner.databinding.FragmentWeekOverviewBinding
 class WeekOverviewFragment : Fragment() {
 
     private var _binding: FragmentWeekOverviewBinding? = null
+
+    private lateinit var weekdayListRecyclerView: RecyclerView
+
+    private lateinit var weekdayListAdapter: WeekdayListAdapter
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -30,6 +36,13 @@ class WeekOverviewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        weekdayListRecyclerView = binding.weekdayListRecyclerView.let {
+            it.layoutManager = LinearLayoutManager(binding.root.context)
+            weekdayListAdapter = WeekdayListAdapter(getInitialWeekdays())
+            it.adapter = weekdayListAdapter
+            it
+        }
     }
 
     override fun onStart() {
@@ -41,5 +54,17 @@ class WeekOverviewFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getInitialWeekdays(): MutableList<Weekday> {
+        return mutableListOf(
+            Weekday(getString(R.string.monday), System.currentTimeMillis()),
+            Weekday(getString(R.string.tuesday), System.currentTimeMillis()),
+            Weekday(getString(R.string.wednesday), System.currentTimeMillis()),
+            Weekday(getString(R.string.thursday), System.currentTimeMillis()),
+            Weekday(getString(R.string.friday), System.currentTimeMillis()),
+            Weekday(getString(R.string.saturday), System.currentTimeMillis()),
+            Weekday(getString(R.string.sunday), System.currentTimeMillis())
+        )
     }
 }
--- app/src/main/java/dev/jefvda/smartplanner/WeekdayListAdapter.kt
+++ app/src/main/java/dev/jefvda/smartplanner/WeekdayListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class WeekdayListAdapter(var weekdayList: MutableList<Weekday>): RecyclerView.Adapter<WeekdayViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeekdayViewHolder {
         return WeekdayViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.weekday_list_item, parent, false))
     }
