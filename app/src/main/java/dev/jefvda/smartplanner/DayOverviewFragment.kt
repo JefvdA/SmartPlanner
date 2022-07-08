462fc17f99ae00bd5328338dbe67f1d1263631e9
JefvdA <jef.v.d.a@live.be>
2022-07-08 10:59:15 +0200

    feat: update toolbar title with correct date
    The toolbar title on the DayOverviewFragment.kt is now updated with the correct date that was passed as argument to the fragment

--- app/src/main/java/dev/jefvda/smartplanner/DayOverviewFragment.kt
+++ app/src/main/java/dev/jefvda/smartplanner/DayOverviewFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.navArgs
 import dev.jefvda.smartplanner.databinding.FragmentDayOverviewBinding
 
 /**
@@ -13,6 +15,9 @@ import dev.jefvda.smartplanner.databinding.FragmentDayOverviewBinding
 class DayOverviewFragment : Fragment() {
 
     private var _binding: FragmentDayOverviewBinding? = null
+    private val args: DayOverviewFragmentArgs by navArgs()
+
+    private lateinit var weekday: Weekday
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +27,16 @@ class DayOverviewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentDayOverviewBinding.inflate(inflater, container, false)
-        return binding.root
+        weekday = args.weekday
 
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.day_overview_toolbar_title, weekday.day, convertCalendarToDateString(weekday.calendar))
     }
 
     override fun onDestroyView() {
